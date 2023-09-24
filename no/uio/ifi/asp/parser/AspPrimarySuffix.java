db07cfeafe8985e985ad2cf5d90b14a182d548c4
Wale Belal Sayed <walebs@ulrik.uio.no>
2023-09-24 12:53:38 +0200

    Update AspPrimarySuffix.java

--- no/uio/ifi/asp/parser/AspPrimarySuffix.java
+++ no/uio/ifi/asp/parser/AspPrimarySuffix.java
@@ -9,18 +9,17 @@ abstract class AspPrimarySuffix extends AspSyntax {
     }
 
     static AspPrimarySuffix parse(Scanner s) {
-        AspPrimarySuffix aps = null;
+        AspPrimarySuffix aps = null;            //TODO: Skal den ikke lagres som en lokal variabel? ^^^ liksom?
         switch (s.curToken().kind) {
+            case leftParToken:
+                aps = AspArguments.parse(s);    //Flytta denne casen over bare fordi rekkefølge er viktig for meg (ocd-problem asså!)
+                break;
             case leftBracketToken:
                 aps = AspSubscription.parse(s);
-                break;
-            case leftParToken:
-                aps = AspArguments.parse(s);
                 break;
         default:
             parserError("Expected a primary suffix but found a " + s.curToken().kind + "!", s.curLineNum());
         }
         return aps;
     }
-
 }
